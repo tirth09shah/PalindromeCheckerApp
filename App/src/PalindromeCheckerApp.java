dc6023686bf7a8cb1c0cc89a27349e5a68382564
AnuragS-2025 <as9771@srmist.edu.in>
2026-03-12 06:53:37 +0530

    UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+
+public class PalindromeCheckerApp{
+    public static void main(String[]args){
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to process palindrome checks...");
+    }
+}
+
